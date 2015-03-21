22d5506662fdc1c102ad4bda5b10b2e6ae0dc580
harper <harper@agideo.com>
2015-03-21 23:12:27 +0800

    about us

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -24,7 +24,7 @@ describe "Static Pages" do
   describe "About page" do 
 
     it "should have the content 'About Us" do 
-      visit '/static_pages'
+      visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
   end
