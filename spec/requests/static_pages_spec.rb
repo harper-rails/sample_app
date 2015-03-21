58ae20270e4eee53d754aa78e7e33ec9497ee912
harper <harper@agideo.com>
2015-03-21 18:06:35 +0800

    about

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,10 @@
 require 'spec_helper'
 
+
 describe "Static Pages" do
+
   describe "Home Page" do 
+
     it "should have the content 'Sample App" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
@@ -10,9 +13,20 @@ describe "Static Pages" do
 
 
   describe "Help page" do 
+
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
   end
+
+
+  describe "About page" do 
+
+    it "should have the content 'About Us" do 
+      visit '/static_pages'
+      expect(page).to have_content('About Us')
+    end
+  end
+
 end
