ba021d43b69d5db3a128622985d59fc22a041349
harper <harper@agideo.com>
2015-03-21 17:25:06 +0800

    spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
-describe 'Static pages' do
-  describe 'Home page' do 
-    it "should have the content 'Sample App'" do 
+
+describe "Static Pages" do
+  describe "Home Page" do 
+    it "should have the content 'Sample App" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
